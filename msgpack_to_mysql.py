9931d5de5604c02bc2bd53cd7f0fce87c605fdc4
Jude Southworth <jude@jellywx.co.uk>
2018-07-12 17:03:54 +0000

    patches to the script

--- msgpack_to_mysql.py
+++ msgpack_to_mysql.py
@@ -6,8 +6,14 @@ from models import Server, session
 with open('data.mp', 'rb') as f:
     data = msgpack.unpackb(zlib.decompress(f.read()), encoding='utf8')
 
+ids = []
 for guild in data:
-    s = Server(id=guild['id'], prefix=guild['prefix'], sounds=guild['sounds'], roles=guild['roles'])
+    if guild['id'] in ids:
+        print('dup')
+        continue
+    ids.append(guild['id'])
+
+    s = Server(id=guild['id'], prefix=guild['prefix'][0:5], sounds=guild['sounds'], roles=guild['roles'])
     session.add(s)
 
 session.commit()
