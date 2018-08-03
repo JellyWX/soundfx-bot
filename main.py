40331035880e6c0e4a38d78bca82a2e75d922b4e
jude-LafiteIII <judewrs@gmail.com>
2018-08-04 00:23:03 +0100

    fixed up the force download stuff for the new method

--- main.py
+++ main.py
@@ -468,7 +468,28 @@ You have {} sounds (using {})
                 if voice.is_playing():
                     voice.stop()
 
-                voice.play(discord.FFmpegPCMAudio(s.url))
+                if self.force_download:
+                    downloaded = [int(f) for f in os.listdir('SOUNDS')]
+
+                    if s.id in downloaded:
+                        print('Sound cached, playing from file...')
+                        voice.play(discord.FFmpegPCMAudio('SOUNDS/{}'.format(s.id)))
+
+                    else:
+                        voice.play(discord.FFmpegPCMAudio(s.url))
+
+                        print('Sound not cached, attempting to cache...')
+
+                        async with aiohttp.ClientSession() as csession:
+                            async with csession.get(s.url) as resp:
+                                t = await resp.read()
+                                with open('SOUNDS/{}'.format(s.id), 'wb') as f:
+                                    f.write(t)
+
+                else:
+                    voice.play(discord.FFmpegPCMAudio(s.url))
+
+                s.last_time = time.time()
 
 
     async def stop(self, message, stripped, server):
@@ -528,8 +549,6 @@ You have {} sounds (using {})
 
         else:
             await message.channel.send('Couldn\'t find sound by name `{}`!'.format(stripped))
-
-
 
 
     async def unlink(self, message, stripped, server):
